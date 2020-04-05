d20d3174d40ac976fd5028e06cc78a1c84672b0d
alexeevaelena06 <alexeevaelena06@gmail.com>
2020-04-05 20:38:17 +0300

    update les_3_task_9.py

--- lesson_3/les_3_task_9.py
+++ lesson_3/les_3_task_9.py
@@ -5,7 +5,9 @@
 # В задачах 3, 4, 5, 6, 9, если искомый элемент(ы) встречается в массиве несколько раз,
 # используйте один любой по вашему выбору.
 
-from random import random
+import random
+
+# 1-ой вариант
 
 M = 10
 N = 5
@@ -13,7 +15,7 @@ a = []
 for i in range(N):
     b = []
     for j in range(M):
-        n = int(random() * 200)
+        n = int(random.random() * 200)
         b.append(n)
         print(f'  {n}', end='')
     a.append(b)
@@ -28,3 +30,24 @@ for j in range(M):
     if mn > mx:
         mx = mn
 print("Максимальный среди минимальных: ", mx)
+
+
+# 2-ой вариант
+
+SIZE = 5
+matrix = [[random.randint(0, 100) for _ in range(SIZE)] for _ in range(SIZE)]
+
+for line in matrix:
+    print(*line, sep="\t")
+
+max_=matrix[0][0]
+for j in range(SIZE):
+    min_ = matrix[0][j]
+    for i in range(SIZE):
+        if matrix[i][j] < min_:
+            min_ = matrix[i][j]
+    if min_ > max_:
+        max_ = min_
+print(f"Max in min = {max_}")
+
+
